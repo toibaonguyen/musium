6eab2c7dec53a61e0785f833c3f9ab616bb262d0
Bao <toibaonguyen@gmail.com>
2024-05-12 16:29:02 +0700

    modify relation ship of post and notification

---
+++ Migrations/20240512092605_modify-relationship-of-post-and-notification.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class modifyrelationshipofpostandnotification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PostNotifications_PostId",
+                table: "PostNotifications");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostNotifications_PostId",
+                table: "PostNotifications",
+                column: "PostId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PostNotifications_PostId",
+                table: "PostNotifications");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostNotifications_PostId",
+                table: "PostNotifications",
+                column: "PostId",
+                unique: true);
+        }
+    }
+}

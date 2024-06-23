124095aa09b1e8d65bd920a2a2b6143133b1be39
Trieu Luu <trieukim1410@gmail.com>
2024-06-23 17:27:53 +0700

    postUI

--- mobile/src/screens/PostCreate/Post.jsx
+++ mobile/src/screens/PostCreate/Post.jsx
@@ -1,28 +1,80 @@
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, Image, SafeAreaView, TextInput, KeyboardAvoidingView, Button, TouchableOpacity, Pressable } from 'react-native'
 import React from 'react'
 import avt from '../../../assets/images/hero2.jpg'
+import { isFulfilled } from '@reduxjs/toolkit';
+// import { Button } from 'react-native-paper';
 
-const Post = () => {
+const Post = ({ onPress, title }) => {
+  const [value, onChangeText] = React.useState();
   return (
-    <SafeAreaView>
-      <View style={{flexDirection : 'row'}}>
+    <SafeAreaView style={{ backgroundColor: 'white', height: '100%' }}>
+      <View style={{ flexDirection: 'row' }}>
         <Image
+          style={{
+            width: 40,
+            height: 40,
+            margin: 12,
+            borderRadius: 70
+          }}
+          source={avt} />
+        <Text style={{ fontSize: 20, color: 'black', textAlignVertical: 'center', marginLeft: 10 }}>Nguyen Van A</Text>
+        <Pressable style={{
+          alignItems: 'flex-end',
+          justifyContent: 'center',
+          marginVertical: 10,
+          paddingHorizontal: 12,
+          borderRadius: 10,
+          // elevation: 300,
+          marginStart: 130,
+          marginRight: 10,
+          backgroundColor: '#1AA3FC',
+        }} onPress={onPress}>
+          <Text style={{
+            fontSize: 14,
+            // lineHeight: 14,
+            fontWeight: 'bold',
+            letterSpacing: 0.25,
+            color: 'white',
+          }}>Post</Text>
+        </Pressable>
+      </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'android' ? 'padding' : 'height'}
         style={{
-          width: 60, 
-          height: 60,
-          margin: 8,
-          borderRadius: 70
-        }}
-        source={avt}/>
+          backgroundColor: 'white',
+          borderBottomColor: '#000000',
+          height: '100%'
+        }}>
+        <TextInput
+          editable
+          multiline
+          inputMode='text'
+          placeholder='Share your thoughts...'
+          onChangeText={text => onChangeText(text)}
+          value={value}
+          style={{ paddingLeft: 20 }}
+        />
 
         
-          <Text style={{fontSize: 20, color: 'black' }}>Nguyen Van A</Text>
-      </View>
-        
+      </KeyboardAvoidingView>
+
+
     </SafeAreaView>
   )
 }
-
+// const styles = StyleSheet.create({
+//   input: {
+//     height: 500,
+//     margin: 12,
+//     borderWidth: 1,
+//     padding: 10,
+//   },
+// });
 export default Post
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})

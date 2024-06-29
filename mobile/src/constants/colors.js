bc28bb0b35fc3fcfc4ea594449ddd7e56fce9299
Trieu Luu <trieukim1410@gmail.com>
2024-06-30 01:58:25 +0700

    update constant + context

---
+++ mobile/src/constants/colors.js
@@ -0,0 +1,16 @@
+export const COLORS = {
+    primary: '#00C2CB',
+    secondary: '#44D7DD',
+    text: '#fff',
+    white: '#fff',
+    grey: '#8A9A9D',
+    background: '#121111',
+    bottomTabBar: '#0F0F0F',
+    itemBackground: '#4263691a',
+    lightBlack: '#292929',
+    top1: '#4a90e2',
+    top2: '#1DC186',
+    top3: '#E35050',
+    icon: '#B9ADA1',
+    headerBlack: '#212121'
+}
